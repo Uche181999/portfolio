028838e22694cb916cf3a2a395e785b844f0ec48
uche181999 <uche181999@gmail.com>
2024-07-19 14:39:14 +0100

    almost through

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
-document.getElementById('contact-form').addEventListener('submit', function (event)
+let currentIndex = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slides img').length;
+
+function showNextSlide()
 {
-    event.preventDefault();
+    currentIndex++;
+    if (currentIndex >= totalSlides)
+    {
+        currentIndex = 0;
+    }
+    let newTransformValue;
 
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
+    if (window.matchMedia("(max-width: 480px)").matches)
+    {
+        newTransformValue = -currentIndex * 100; // Full width for each image
+    } else if (window.matchMedia("(max-width: 768px)").matches)
+    {
+        newTransformValue = -currentIndex * 50; // Adjust accordingly for medium screens
+    } else
+    {
+        newTransformValue = -currentIndex * (30 / totalSlides); // Default logic for larger screens
+    }
 
-    alert(`Thank you, ${name}! Your message has been received. We'll contact you at ${email} shortly.`);
+    slides.style.transform = `translateX(${newTransformValue}%)`;
+}
+
+setInterval(showNextSlide, 5000);
+
+document.getElementById('downloadBtn').addEventListener('click', function ()
+{
+    const link = document.createElement('a');
+    link.href = 'img/Uche_cv.pdf';
+    link.download = 'Uche_cv.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 });
